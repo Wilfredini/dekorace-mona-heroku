10c727322408cc3665b8ef86cf9eb55178d5eb9f
Tomas Charvat <charvi.tomas@seznam.cz>
2022-11-13 14:34:44 +0100

    commit

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express;
 const path = require("path");
 const port = process.env.PORT || 5432;
-
+const sslRedirect = require("heroku-ssl-redirect").default;
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
   app.get("*", (req, res) => {
