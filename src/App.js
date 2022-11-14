d432c3de7887e64fb27e2f6becdbca163f537203
Tomas Charvat <charvi.tomas@seznam.cz>
2022-11-14 20:29:11 +0100

    test commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { lazy, Suspense } from "react";
 import "./App.scss";
 import Navbar from "./components/Layout/Navbar";
-
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Layout/Footer";
 
-const Home = lazy(() => import("./pages/Home"));
-const About = lazy(() => import("./pages/About"));
+const Home = React.lazy(() => import("./pages/Home"));
+const About = React.lazy(() => import("./pages/About"));
 const Services = lazy(() => import("./pages/Services"));
 const Gallery = lazy(() => import("./pages/Gallery"));
 const Contact = lazy(() => import("./pages/Contact"));
@@ -15,23 +14,21 @@ const Login = lazy(() => import("./pages/Login"));
 
 function App() {
   return (
-    <div className="App">
-      <Router>
+    <>
+      <div className="App">
         <Navbar />
         <Routes>
-          <Suspense fallback={<h1>Loading...</h1>}>
-            <Route exact path="/Home" element={<Home />} />
-            <Route path="/About" element={<About />} />
-            <Route path="/Services" element={<Services />} />
-            <Route path="/Gallery" element={<Gallery />} />
-            <Route path="/Contact" element={<Contact />} />
-            <Route path="/Login" element={<Login />} />
-            <Route path="/Register" element={<Register />} />
-          </Suspense>
+          <Route exact path="/Home" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Services" element={<Services />} />
+          <Route path="/Gallery" element={<Gallery />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
         </Routes>
         <Footer />
-      </Router>
-    </div>
+      </div>
+    </>
   );
 }
 
