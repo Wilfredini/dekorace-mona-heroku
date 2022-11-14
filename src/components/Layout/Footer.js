270faa4b852cf9c695f204327b30adbc7b2ff3e3
Tomas Charvat <charvi.tomas@seznam.cz>
2022-11-14 02:18:43 +0100

    footer debug

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -5,22 +5,22 @@ function Footer() {
     <div className="footer pt-sm-5 pt-3">
       <nav className="footer-nav">
         <ul className="footer-menu text-light text-center">
-          <li className="nav-item">
+          <li className="nav-item py-3">
             <a href="/Home">Domů</a>
           </li>
-          <li className="nav-item">
+          <li className="nav-item py-3">
             <a href="/About">O mně</a>
           </li>
-          <li className="nav-item">
+          <li className="nav-item py-3">
             <a href="/Services">Služby</a>
           </li>
-          <li className="nav-item">
+          <li className="nav-item py-3">
             <a href="/Products">Produkty</a>
           </li>
-          <li className="nav-item">
+          <li className="nav-item py-3">
             <a href="/Contact">Kontakt</a>
           </li>
-          <li className="nav-item">
+          <li className="nav-item py-3">
             <a href="/Login">Přihlásit</a>
           </li>
         </ul>
