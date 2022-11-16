8c9945265a2f7fea04eefb24f540ae928aa87ace
Tomas Charvat <charvi.tomas@seznam.cz>
2022-11-16 20:21:25 +0100

    router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy } from "react";
 import "./App.scss";
 import Navbar from "./components/Layout/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
 import Footer from "./components/Layout/Footer";
 
-const Home = lazy(() => import("./pages/Home"));
-const About = lazy(() => import("./pages/About"));
-const Services = lazy(() => import("./pages/Services"));
-const Gallery = lazy(() => import("./pages/Gallery"));
-const Contact = lazy(() => import("./pages/Contact"));
-const Register = lazy(() => import("./pages/Register"));
-const Login = lazy(() => import("./pages/Login"));
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Gallery from "./pages/Gallery";
+import Contact from "./pages/Contact";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
