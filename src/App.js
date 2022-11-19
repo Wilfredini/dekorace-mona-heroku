0f54f08457d20e9b08af0198af566f596cd3d78a
Tomas Charvat <charvi.tomas@seznam.cz>
2022-11-19 02:22:10 +0100

    debug router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.scss";
 import Navbar from "./components/Layout/Navbar";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Layout/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -11,22 +11,24 @@ import Contact from "./pages/Contact";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 
-function App() {
-  return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route exact path="/Home" element={<Home />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Services" element={<Services />} />
-        <Route path="/Gallery" element={<Gallery />} />
-        <Route path="/Contact" element={<Contact />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Register" element={<Register />} />
-      </Routes>
-      <Footer />
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Services" element={<Services />} />
+          <Route path="/Gallery" element={<Gallery />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+        </Routes>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
