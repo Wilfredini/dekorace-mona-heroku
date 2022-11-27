d8bdb774bf1b37991c95e7d0c3cc09f3df944328
Tomas Charvat <charvi.tomas@seznam.cz>
2022-11-27 17:34:59 +0100

    infowindow add

--- src/pages/Poloha.js
+++ src/pages/Poloha.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { useMemo } from "react";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import ImgPoloha from "../images/Poloha.jpg";
+
+import {
+  GoogleMap,
+  useLoadScript,
+  MarkerF,
+  InfoWindow,
+} from "@react-google-maps/api";
 
 export default function Poloha() {
-  const center = { lat: 48.984058499732896, lng: 17.328763757527735 };
-
+  const center = { lat: 48.983929, lng: 17.328741 };
+  const markerCoor = { lat: 48.983929, lng: 17.328741 };
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
   });
@@ -14,11 +20,27 @@ export default function Poloha() {
   function Map() {
     return (
       <GoogleMap
-        zoom={10}
+        zoom={18}
         center={center}
         mapContainerClassName="map-container"
       >
-        <Marker position={center} />
+        <MarkerF title="Dekorace Mona" position={markerCoor}>
+          <InfoWindow position={center}>
+            <div className="d-flex flex-column justify-content-center align-items-center">
+              <h4 className="pb-2">Dekorace Mona</h4>
+              <img
+                className="pb-2"
+                src={ImgPoloha}
+                alt="Info foto"
+                width="100px"
+                height="70px"
+              />
+              <a href="https://www.google.com/maps/place/Velkomoravsk%C3%A1+503,+696+85+Moravsk%C3%BD+P%C3%ADsek/@48.9839247,17.3265536,17z/data=!4m13!1m7!3m6!1s0x47133c5750596283:0x9503b8c07f93458f!2sVelkomoravsk%C3%A1+503,+696+85+Moravsk%C3%BD+P%C3%ADsek!3b1!8m2!3d48.9839212!4d17.3287423!3m4!1s0x47133c5750596283:0x9503b8c07f93458f!8m2!3d48.9839212!4d17.3287423?hl=cs-CZ">
+                Zobrazit na mapách Google
+              </a>
+            </div>
+          </InfoWindow>
+        </MarkerF>
       </GoogleMap>
     );
   }
